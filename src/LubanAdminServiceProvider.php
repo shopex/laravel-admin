a29f7f2070be9912ed7998e148e046aa8660bed6
Wanglei <flaboy.cn@gmail.com>
2017-08-17 18:19:22 +0800

    merge adminui

--- src/LubanAdminServiceProvider.php
+++ src/LubanAdminServiceProvider.php
@@ -21,20 +21,18 @@ class LubanAdminServiceProvider extends ServiceProvider
      */
     public function boot(\Illuminate\Routing\Router $router)
     {
+        $this->loadViewsFrom(__DIR__.'/../publish/views', 'admin');
+
         $this->publishes([
             __DIR__ . '/../publish/Middleware/' => app_path('Http/Middleware'),
             __DIR__ . '/../publish/migrations/' => database_path('migrations'),
             __DIR__ . '/../publish/Model/' => app_path(),
             __DIR__ . '/../publish/Controllers/' => app_path('Http/Controllers'),
             __DIR__ . '/../publish/resources/' => base_path('resources'),
+            __DIR__ . '/../publish/views' => base_path('resources/views/vendor/admin'),
+            __DIR__ . '/../publish/assets' => base_path('resources/assets/vendor/admin'),
             // __DIR__ . '/../publish/crudgenerator.php' => config_path('crudgenerator.php'),
         ]);
-
-        $this->publishes([
-            __DIR__ . '/views' => base_path('resources/views/vendor/admin'),
-        ], 'views');
-
-        $this->loadViewsFrom(__DIR__ . '/views', 'admin');
     }
 
     /**
