8a7292b929ae01e6d50281b2a31789ee88a8a56a
Wanglei <flaboy.cn@gmail.com>
2017-11-13 14:51:56 +0800

    m

--- publish/resources/views/desktop.blade.php
+++ publish/resources/views/desktop.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>{{$app_name}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="admin-baseurl" content="{{ url('/') }}">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
   </head>
