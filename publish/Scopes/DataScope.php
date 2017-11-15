6ec4c80aec8da79e6b4380d9a9bfab804df87e6a
张学会 <zhangxuehui@shopex.cn>
2017-11-15 11:29:27 +0800

    add data scope

---
+++ publish/Scopes/DataScope.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Shopex\LubanAdmin\Permission\Data;
+class DataScope implements Scope
+{
+	/**
+	 * 初始化模型
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public function __construct($modelName)
+	{
+        $this->modelName = $modelName;
+	}
+    /**
+     * 应用作用域
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        if (auth()->user()->isAdmin()) {
+            return $builder;
+        }
+        $data = new Data($this->modelName);
+        $result = $data->check();
+        if ($result) {
+            foreach ($result as $role_id => $whereGroup) {
+                 $builder->where(function ($builder) use ($whereGroup) {
+                    foreach ($whereGroup as $key => $group) {
+                        foreach ($group as $field => $value) {
+                            $builder->orWhereIn($field, $value);
+                        }
+                    }
+                });
+            }
+        }
+        return $builder;
+    }
+}
