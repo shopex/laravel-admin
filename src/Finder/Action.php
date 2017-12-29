4b0fd4aaa9fb2b6c2c7631ea1d02b09f50098134
张学会 <zhangxuehui@shopex.cn>
2017-12-29 11:26:12 +0800

    add action modal custom

--- src/Finder/Action.php
+++ src/Finder/Action.php
@@ -10,6 +10,8 @@ class Action{
 	public $url;
 	public $target;
 	public $confirm;
+	public $modal_height;
+	public $modal_width;
 	public $hidden = ['handle'];
 
 	function newWindow(){
@@ -31,9 +33,18 @@ class Action{
 		$this->target = '#modal-large';
 		return $this;
 	}
+
+	function modalCustom($height,$width){
+		$this->modal_height = $height;
+		$this->modal_width= $width;
+		$this->target = '#modal-custom';
+		return $this;
+	}
+
 	function confirm($msg)
 	{
 		$this->confirm = $msg;
 		return $this;
 	}
+
 }
